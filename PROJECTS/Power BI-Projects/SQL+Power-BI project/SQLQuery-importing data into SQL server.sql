83a54d53a1f0960bede8f28b8f3ca52b007ec0e8
Dat-Anubhav <anubhavsrivastavbst@gmail.com>
2026-01-18 23:50:39 +0530

    Add SQL+Power-BI Project files

---
+++ PROJECTS/Power BI-Projects/SQL+Power-BI project/SQLQuery-importing data into SQL server.sql
@@ -0,0 +1,12 @@
+
+--Creating a new database for the Sql+Power-Bi project--
+
+Create database Insurance
+
+--imported data into sql server
+
+--Checking the imported data
+
+Select * from [dbo].[InsuranceData]
+
+--Next step is to load this data into Microsoft Power-BI
